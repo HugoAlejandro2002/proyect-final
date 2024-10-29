4d2b4a0c5bfb485feceb8b01be1a45c2d350d489
HugoAlejandro2002 <apazahuaychohugoalejandro@gmail.com>
2024-10-29 12:10:18 -0400

    feat: improved script for source images

--- image_downloader.py
+++ image_downloader.py
@@ -1,17 +1,23 @@
-from icrawler.builtin import BingImageCrawler, BaiduImageCrawler
+from icrawler.builtin import BingImageCrawler
+import os
 
-def download_images(search_term, num_images, download_path, source='bing'):
-    if source == 'bing':
-        crawler = BingImageCrawler(storage={'root_dir': download_path})
-    elif source == 'baidu':
-        crawler = BaiduImageCrawler(storage={'root_dir': download_path})
-    else:
-        raise ValueError("Fuente no válida. Usa 'bing' o 'baidu'.")
+def filter_images_by_extension(download_path, allowed_extensions):
+    """Elimina archivos que no tengan la extensión permitida."""
+    for filename in os.listdir(download_path):
+        if not any(filename.endswith(ext) for ext in allowed_extensions):
+            os.remove(os.path.join(download_path, filename))
 
-    # Realiza la búsqueda y descarga las imágenes
+def download_images(search_term, num_images, download_path, allowed_extensions=['.jpg', '.png']):
+
+    crawler = BingImageCrawler(storage={'root_dir': download_path})
+    
     crawler.crawl(keyword=search_term, max_num=num_images, file_idx_offset=0)
 
+    filter_images_by_extension(download_path, allowed_extensions)
 
-download_images("fat male front body mirror selfie", 50, "./fat_images", source='bing')
-# download_images("fat male front body mirror selfie", 50, "./fat_images_2", source='baidu')
-
+download_images(
+    "fat male frontal body", 
+    50, 
+    "./fat_images2", 
+    allowed_extensions=['.jpg', '.png','jpeg']
+)
